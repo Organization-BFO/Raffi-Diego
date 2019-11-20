69b5658fa5807e296199ab03d134f65bba18cd11
Diego Schmid <diego.schmid@lernende.bfo-vs.ch>
2019-11-20 14:40:49 +0100

    Aufgabe 6

---
+++ Aufgabe 6.php
@@ -0,0 +1,23 @@
+<?php
+
+// FILE: library/calc.php
+
+
+function add($value1, $valu2)
+{
+	return $value +$value2;
+}
+
+
+function addWithArray($valueArray){
+$sum =0;
+
+foreach ($valueArray_ as $value)
+$sum + value1 + value2
+
+
+return $sum;
+}
+
+
+>?
