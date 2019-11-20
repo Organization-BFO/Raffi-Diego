b94801b2830374d85e4539ea937006e624909369
you@example.com <you@example.com>
2019-11-20 14:35:50 +0100

    made Aufgabe5

---
+++ Aufgabe5.php
@@ -0,0 +1,17 @@
+<?php
+$letters = ['A','B','C','D','E','F','G','H','I'];
+ foreach($letters as $key => $letter){
+	 
+	
+	if($key %3==0 && $key !=0) {
+		printf("<br>");
+	
+	
+	}
+	 echo $letter;
+	 
+ }
+ 
+ 
+?>
+
