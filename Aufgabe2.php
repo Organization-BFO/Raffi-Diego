ee1b975449b68591d996518e4fd255d4cfd9ac78
Diego Schmid <diego.schmid@lernende.bfo-vs.ch>
2019-11-20 14:21:36 +0100

    Aufgabe 1/2

---
+++ Aufgabe2.php
@@ -0,0 +1,63 @@
+<?php
+
+
+
+
+
+
+
+
+?>
+<head>
+</head>
+
+
+
+
+<body>
+	
+	
+	
+	
+	<table>
+	
+	
+	<tr>
+
+
+<td>
+
+Erste Zeile 
+Zweite  Zeile
+Dritte Zeile
+
+
+
+</td>
+
+
+
+
+
+
+	<tr>
+	
+	
+	
+	
+	
+	</table>
+	
+	
+	
+	
+</body>
+
+
+
+
+
+
+
+
+<?>
